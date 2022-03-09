5315fc6bf636226c80abbfd6914713e1f9b054b9
Lucas <undertalefan812@gmail.com>
2022-03-09 13:47:35 -0500

    Gave proper classnames to buttons

--- src/Components/Shop.tsx
+++ src/Components/Shop.tsx
@@ -9,27 +9,27 @@ function Shop() {
                 <h2 className='head'>Shop</h2>
                 <div>
                     <p>2x Multiplier</p>
-                    <button className='buy-btn'>Buy</button>
+                    <button className='2x'>Buy</button>
                 </div>
                 <div>
                     <p>4x Multiplier</p>
-                    <button className='buy-btn'>Buy</button>
+                    <button className='4x'>Buy</button>
                 </div>
                 <div>
                     <p>Mr.Zom</p>
-                    <button className='buy-btn'>Buy</button>    
+                    <button className='zom'>Buy</button>    
                 </div>
                 <div>
                     <p>Autoclicker</p>
-                    <button className='buy-btn'>Buy</button>    
+                    <button className='autoclicker'>Buy</button>    
                 </div>
                 <div>
                     <p>6x Multiplier</p>
-                    <button className='buy-btn'>Buy</button>    
+                    <button className='6x'>Buy</button>    
                 </div>
                 <div>
                     <p>10000000x Multiplier</p>
-                    <button className='buy-btn'>Buy</button>    
+                    <button className='infinite'>Buy</button>    
                 </div>
             </div>
         </div>
