50be3145fe081f05427a43732b439a92457b35e1
Lucas <undertalefan812@gmail.com>
2022-03-11 08:07:50 -0500

    Moved shop into app component due to frustration

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,23 +2,54 @@ import React, { useContext } from 'react'
 import './App.scss'
 import './App'
 import { useState } from 'react'
-import { ShopContext } from './Shop'
+import './Shop.scss'
 
-
+let clickMod = 1
 
 function App() {
 
-    let countMod = useContext(ShopContext)
-
-    const [count, setCount] = useState(0)
+    let [click, setclick] = useState(0)
 
     return(
         <div>
             <h1>Welcome to my Clicker Game</h1>
             <div className="spacing">
-                <h3>Count: {count}</h3>
-                <button className="btn" onClick={() => setCount(count + countMod)} >Click here!</button>
+                <h3>Clicks: {click}</h3>
+                <button className="btn" onClick={() => setclick(click + clickMod)} >Click here!</button>
                 <button className="btn">Shop</button>
+            </div>
+            <div className='shop'>
+                <h2 className='head'>Shop</h2>
+                <div>
+                    <p>2x Multiplier</p>
+                    <p>25 clicks</p>
+                    <button onClick={() => {
+                        if (click >= 25) {
+                            clickMod *= 2
+                            setclick(click -= 25)
+                        }
+                    }}>Buy</button>
+                </div>
+                <div>
+                    <p>4x Multiplier</p>
+                    <button className='4x'>Buy</button>
+                </div>
+                <div>
+                    <p>Mr.Zom</p>
+                    <button className='zom'>Buy</button>    
+                </div>
+                <div>
+                    <p>Autoclicker</p>
+                    <button className='autoclicker'>Buy</button>    
+                </div>
+                <div>
+                    <p>6x Multiplier</p>
+                    <button className='6x'>Buy</button>    
+                </div>
+                <div>
+                    <p>10000000x Multiplier</p>
+                    <button className='infinite'>Buy</button>    
+                </div>
             </div>
         </div>
     )
